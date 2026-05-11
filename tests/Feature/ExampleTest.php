c1750fa804d3ce712f85da4402637244368efd75
naostudy0 <nao.study0@gmail.com>
2026-05-11 17:05:13 +0900

    chore: サンプルテスト修正

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,8 +2,9 @@
 
 use Inertia\Testing\AssertableInertia as Assert;
 
+// TODO: pestの確認のためのテストコード。後で削除する。
 it('returns a successful response', function () {
     $this->get('/')
         ->assertOk()
-        ->assertInertia(fn (Assert $page) => $page->component('Top')->has('errors'));
+        ->assertInertia(fn (Assert $page) => $page->component('Top', false)->has('errors'));
 });
